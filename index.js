ba4f7e2fa3dabde5a7b79e7a43f74c0a0a12fe01
FlorenciaRomano <florromanoyt@gmail.com>
2024-05-31 01:53:49 -0300

    traduccion total

--- index.js
+++ index.js
@@ -16,3 +16,29 @@ cards.forEach(card => {
         card.children[1].children[2].classList.remove('showP')
     })
 })
+
+// CAMBIO DE IDIOMA
+
+const toggleTheme = document.getElementById("toggle-theme");
+const toggleIcon = document.getElementById("toggle.icon");
+const toggleText = document.getElementById("toggle-text");
+
+const flagsElement = document.getElementById("flags")
+
+const textsToChange = document.querySelectorAll("[data-section]");
+
+const changeLanguage = async language=>{
+    const requestJson = await fetch(`./languages/${language}.json`);
+    const texts = await requestJson.json();
+    
+    for(const textToChange of textsToChange){
+       const section = textToChange.dataset.section;
+       const value = textToChange.dataset.value;
+       
+       textToChange.innerHTML = texts[section][value];
+    }
+};
+
+flagsElement.addEventListener("click", (e) => {
+    changeLanguage(e.target.parentElement.dataset.language);
+});
